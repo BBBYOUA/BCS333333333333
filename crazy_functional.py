cce146d85e5b6b520d7c026d11bb47e851e51cbb
qingxu fu <505030475@qq.com>
2023-06-14 12:24:40 +0800

    修改完善注释

--- crazy_functional.py
+++ crazy_functional.py
@@ -1,8 +1,25 @@
 from toolbox import HotReload  # HotReload 的意思是热更新，修改函数插件后，不需要重启程序，代码直接生效
 
-
+# 这个函数负责把任意函数插件装载到WEB界面中去
 def get_crazy_functions():
-    ###################### 第一组插件 ###########################
+    """
+    ========================================================================
+    函数插件的引入示例如下：
+    {
+        "插件显示在WEB界面的名字": {
+            "Color": "stop",                 # 插件的颜色（stop或secondary，前者红色，代表需要读取输入区; 后者白色，代表不读取输入区）
+            "AsButton": False,               # True代表作为`按钮`加入WEB界面，False代表作为`下拉菜单的一员`加入WEB界面
+            "AdvancedArgs": True,            # True代表在选中后需要弹出`高级参数输入区`（例如生成图像插件中，用于输入分辨率）
+            "ArgsReminder": "",              # `高级参数输入区`（如启用）显示给用户的提示文本，例如"分辨率的输入格式是 123x123"
+            "Function": HotReload(函数入口)   # 函数入口。HotReload 的意思是热更新，修改函数插件后，不需要重启程序，代码直接生效。如插件中有全局变量，慎用HotReload
+        }
+    }
+
+
+    ========================================================================
+    第一组插件：最早引入的一批插件
+    ========================================================================
+    """
     from crazy_functions.读文章写摘要 import 读文章写摘要
     from crazy_functions.生成函数注释 import 批量生成函数注释
     from crazy_functions.解析项目源代码 import 解析项目本身
@@ -44,64 +61,63 @@ def get_crazy_functions():
             "Color": "stop",
             "AsButton":False,
             "Function": HotReload(解析ipynb文件),
-            "AdvancedArgs": True, # 调用时，唤起高级参数输入区（默认False）
-            "ArgsReminder": "若输入0，则不解析notebook中的Markdown块", # 高级参数输入区的显示提示
+            "AdvancedArgs": True,  
+            "ArgsReminder": "若输入0，则不解析notebook中的Markdown块", 
         },
         "批量总结Word文档": {
             "Color": "stop",
             "Function": HotReload(总结word文档)
         },
         "解析整个C++项目头文件": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个C项目的头文件)
         },
         "解析整个C++项目（.cpp/.hpp/.c/.h）": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个C项目)
         },
         "解析整个Go项目": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个Golang项目)
         },
         "解析整个Rust项目": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个Rust项目)
         },
         "解析整个Java项目": {
-            "Color": "stop",  # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个Java项目)
         },
         "解析整个前端项目（js,ts,css等）": {
-            "Color": "stop",  # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个前端项目)
         },
         "解析整个Lua项目": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个Lua项目)
         },
         "解析整个CSharp项目": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(解析一个CSharp项目)
         },
         "读Tex论文写摘要": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "Function": HotReload(读文章写摘要)
         },
         "Markdown/Readme英译中": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "Function": HotReload(Markdown英译中)
         },
         "批量生成函数注释": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(批量生成函数注释)
         },
@@ -113,18 +129,22 @@ def get_crazy_functions():
             "Function": HotReload(解析项目本身)
         },
         "[老旧的Demo] 把本项目源代码切换成全英文": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(全项目切换英文)
         },
         "[插件demo] 历史上的今天": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Function": HotReload(高阶功能模板函数)
         },
 
     }
-    ###################### 第二组插件 ###########################
-    # [第二组插件]: 经过充分测试
+
+
+
+    """
+    ========================================================================
+    第二组插件：经过充分测试的插件
+    ========================================================================
+    """
     from crazy_functions.批量总结PDF文档 import 批量总结PDF文档
     # from crazy_functions.批量总结PDF文档pdfminer import 批量总结PDF文档pdfminer
     from crazy_functions.批量翻译PDF文档_多线程 import 批量翻译PDF文档
@@ -143,63 +163,50 @@ def get_crazy_functions():
             "Function": HotReload(批量翻译PDF文档)
         },
         "询问多个GPT模型": {
-            "Color": "stop",    # 按钮颜色
+            "Color": "stop",
             "Function": HotReload(同时问询)
         },
         "[测试功能] 批量总结PDF文档": {
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Function": HotReload(批量总结PDF文档)
         },
-        # "[测试功能] 批量总结PDF文档pdfminer": {
-        #     "Color": "stop",
-        #     "AsButton": False,  # 加入下拉菜单中
-        #     "Function": HotReload(批量总结PDF文档pdfminer)
-        # },
         "谷歌学术检索助手（输入谷歌学术搜索页url）": {
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(谷歌检索小助手)
         },
         "理解PDF文档内容 （模仿ChatPDF）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(理解PDF文档内容标准文件输入)
         },
         "英文Latex项目全文润色（输入路径或上传压缩包）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(Latex英文润色)
         },
         "英文Latex项目全文纠错（输入路径或上传压缩包）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(Latex英文纠错)
         },
         "中文Latex项目全文润色（输入路径或上传压缩包）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(Latex中文润色)
         },
         "Latex项目全文中译英（输入路径或上传压缩包）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(Latex中译英)
         },
         "Latex项目全文英译中（输入路径或上传压缩包）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(Latex英译中)
         },
         "批量Markdown中译英（输入路径或上传压缩包）": {
-            # HotReload 的意思是热更新，修改函数插件代码后，不需要重启程序，代码直接生效
             "Color": "stop",
             "AsButton": False,  # 加入下拉菜单中
             "Function": HotReload(Markdown中译英)
@@ -208,9 +215,12 @@ def get_crazy_functions():
 
     })
 
-    ###################### 第三组插件 ###########################
-    # [第三组插件]: 尚未充分测试的函数插件
 
+    """
+    ========================================================================
+    第三组插件：依赖关系比较复杂的插件 / 测试不充分的新插件
+    ========================================================================
+    """
     try:
         from crazy_functions.下载arxiv论文翻译摘要 import 下载arxiv论文并翻译摘要
         function_plugins.update({
@@ -220,8 +230,7 @@ def get_crazy_functions():
                 "Function": HotReload(下载arxiv论文并翻译摘要)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.联网的ChatGPT import 连接网络回答问题
@@ -232,8 +241,7 @@ def get_crazy_functions():
                 "Function": HotReload(连接网络回答问题)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.解析项目源代码 import 解析任意code项目
@@ -241,13 +249,12 @@ def get_crazy_functions():
             "解析项目源代码（手动指定和筛选源代码文件类型）": {
                 "Color": "stop",
                 "AsButton": False,
-                "AdvancedArgs": True, # 调用时，唤起高级参数输入区（默认False）
-                "ArgsReminder": "输入时用逗号隔开, *代表通配符, 加了^代表不匹配; 不输入代表全部匹配。例如: \"*.c, ^*.cpp, config.toml, ^*.toml\"", # 高级参数输入区的显示提示
+                "AdvancedArgs": True,
+                "ArgsReminder": "输入时用逗号隔开, *代表通配符, 加了^代表不匹配; 不输入代表全部匹配。例如: \"*.c, ^*.cpp, config.toml, ^*.toml\"", 
                 "Function": HotReload(解析任意code项目)
             },
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.询问多个大语言模型 import 同时问询_指定模型
@@ -255,13 +262,12 @@ def get_crazy_functions():
             "询问多个GPT模型（手动指定询问哪些模型）": {
                 "Color": "stop",
                 "AsButton": False,
-                "AdvancedArgs": True, # 调用时，唤起高级参数输入区（默认False）
-                "ArgsReminder": "支持任意数量的llm接口，用&符号分隔。例如chatglm&gpt-3.5-turbo&api2d-gpt-4", # 高级参数输入区的显示提示
+                "AdvancedArgs": True,
+                "ArgsReminder": "支持任意数量的llm接口，用&符号分隔。例如chatglm&gpt-3.5-turbo&api2d-gpt-4", 
                 "Function": HotReload(同时问询_指定模型)
             },
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.图片生成 import 图片生成
@@ -269,13 +275,12 @@ def get_crazy_functions():
             "图片生成（先切换模型到openai或api2d）": {
                 "Color": "stop",
                 "AsButton": False,
-                "AdvancedArgs": True, # 调用时，唤起高级参数输入区（默认False）
-                "ArgsReminder": "在这里输入分辨率, 如256x256（默认）", # 高级参数输入区的显示提示
+                "AdvancedArgs": True,
+                "ArgsReminder": "在这里输入分辨率, 如256x256（默认）", 
                 "Function": HotReload(图片生成)
             },
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.总结音视频 import 总结音视频
@@ -288,8 +293,7 @@ def get_crazy_functions():
                 "Function": HotReload(总结音视频)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.数学动画生成manim import 动画生成
@@ -300,8 +304,7 @@ def get_crazy_functions():
                 "Function": HotReload(动画生成)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.批量Markdown翻译 import Markdown翻译指定语言
@@ -314,8 +317,7 @@ def get_crazy_functions():
                 "Function": HotReload(Markdown翻译指定语言)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.Langchain知识库 import 知识库问答
@@ -328,8 +330,7 @@ def get_crazy_functions():
                 "Function": HotReload(知识库问答)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.Langchain知识库 import 读取知识库作答
@@ -342,8 +343,7 @@ def get_crazy_functions():
                 "Function": HotReload(读取知识库作答)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.Latex输出PDF结果 import Latex英文纠错加PDF对比
@@ -351,36 +351,28 @@ def get_crazy_functions():
             "[功能尚不稳定] Latex英文纠错+LatexDiff高亮修正位置": {
                 "Color": "stop",
                 "AsButton": False,
-                # "AdvancedArgs": True,
-                # "ArgsReminder": "",
                 "Function": HotReload(Latex英文纠错加PDF对比)
             }
         })
         from crazy_functions.Latex输出PDF结果 import Latex翻译中文并重新编译PDF
         function_plugins.update({
-            "[功能尚不稳定] Latex翻译/Arixv翻译+重构PDF": {
+            "Latex翻译/Arixv翻译+重构PDF": {
                 "Color": "stop",
                 "AsButton": False,
-                # "AdvancedArgs": True,
-                # "ArgsReminder": "",
                 "Function": HotReload(Latex翻译中文并重新编译PDF)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     try:
         from crazy_functions.虚空终端 import 终端
         function_plugins.update({
-            "超级终端": {
+            "虚空终端 （用自然语言描述调用其他插件）": {
                 "Color": "stop",
                 "AsButton": False,
-                # "AdvancedArgs": True,
-                # "ArgsReminder": "",
                 "Function": HotReload(终端)
             }
         })
-    except:
-        print('Load function plugin failed')
+    except: print('Load function plugin failed')
 
     return function_plugins
